04ad7d5b2ceae3f0dbfe67286d6ef9e8a784465b
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2022-04-29 15:45:09 -0500

    Quiet composer

--- src/Traits/ComposerPackages.php
+++ src/Traits/ComposerPackages.php
@@ -6,14 +6,14 @@ trait ComposerPackages
 {
     private function setupComposerPackages(): void
     {
-        $this->exec(sprintf('composer require --dev -n %s',
+        $this->exec(sprintf('composer require --dev -n --quiet %s',
             implode(' ', self::COMPOSER_PACKAGES)
         ));
     }
 
     private function removeComposerPackages(): void
     {
-        $this->exec(sprintf('composer remove --dev -n %s',
+        $this->exec(sprintf('composer remove --dev -n --quiet %s',
             implode(' ', self::COMPOSER_PACKAGES)
         ));
     }
